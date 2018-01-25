860ba2f461b788710f117b45277c3aea1b8f62ad
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-25 17:39:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
-# your code goes here
+def begins_with_r(array)
+  i = 0
+  while i < array.size
+    if array[i].start_with?("r") == true
+      i += 1
+    else
+      return false
+    end 
+  end 
+  return true
+end
+
+def contain_a(array)
+ return array.all? {|word| word.start_with?("r")}
+end 
+
+def first_wa(array)
+  i = 0
+  while i < array.size
+    if array[i].start_with?("wa") == true
+      return array[i]
+    else 
+      i += 1
+    end 
+  end 
+end 
