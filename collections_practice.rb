a345848ced637467b0e524153ed039903b0186e5
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-25 18:05:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|word| word.start_with?("r")}
+end
+
+def contain_a(array)
+ array.find_all {|word| word.include?("a")}
+end 
+
+def first_wa(array) 
+  array.find do |word| 
+    word[0..1] == "wa"
+  end 
+end 
+
+def remove_non_strings(array)
+  array.delete_if do |element|
+    element.class != String
+  end 
+end 
